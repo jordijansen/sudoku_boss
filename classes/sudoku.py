c5137d6490a9b8936904d32e71f24e7ce8990a23
Jordi Jansen <jordijansen93@gmail.com>
2015-05-05 10:26:02 +0200

    Sudoku Box

--- classes/sudoku.py
+++ classes/sudoku.py
@@ -11,3 +11,20 @@ class Sudoku:
 
     def set_value(self, row, col, value):
         self.sudoku[row][col] = value
+
+    def validate_row(self, row, value):
+        sudokurow = self.sudoku[row]
+        for position in sudokurow:
+            if position == value:
+                return False
+        return True
+
+    def validate_col(self, col, value):
+        for row in self.sudoku:
+            if row[col] == value:
+                return False
+        return True
+
+    def validate_box(self, row, col, value):
+        return True
+
